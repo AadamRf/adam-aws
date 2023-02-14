4ecd498a63119d556f9cb4ddc983511215522910
AadamRf <rouf.aadam@gmail.com>
2023-02-14 14:22:48 +0100

    Python Employees Adr POST

--- backend-Python/adr.py
+++ backend-Python/adr.py
@@ -1,15 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/v1/employees', methods=['POST'])
-def post_employees():
-    employees = [
-        {'id': 4, 'firstName': 'Josh', 'lastName': 'Dong', 'emailId': 'joshdong@example.com'},
-        {'id': 5, 'firstName': 'Janet', 'lastName': 'Smurf', 'emailId': 'janetsmurf@example.com'},
-        {'id': 6, 'firstName': 'Robert', 'lastName': 'Deniro', 'emailId': 'robertdeniro@example.com'}
-    ]
+employees = [
+    {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+    {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+    {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+]
+
+@app.route('/api/v1/employees', methods=['GET'])
+def get_employees():
     return jsonify(employees)
+
+@app.route('/api/v1/employees', methods=['POST'])
+def add_employee():
+    new_employee = request.json
+    new_employee['id'] = len(employees) + 1
+    employees.append(new_employee)
+    return jsonify(new_employee), 201
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
