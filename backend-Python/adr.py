b6ca37300c775c5421d476c295bde1ac458ab5c2
AadamRf <rouf.aadam@gmail.com>
2023-02-15 14:14:59 +0100

    Enable Cross-Origin

--- backend-Python/adr.py
+++ backend-Python/adr.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 employees = [
     {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
